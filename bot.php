65675c3aea8d7269a9fff648f8540029205d1c8e
NuttyPanu <misterpanukorn@gmail.com>
2018-10-31 20:44:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -142,6 +142,52 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			echo $result . "\r\n";
 		}
+		
+	        //get location
+	        else if ($event['type'] == 'message' && $event['message']['type'] == 'location') {
+         
+		    $latitude = $event['message']['latitude'];
+		    $longitude = $event['message']['longitude'];
+		    $title = $event['message']['title'];
+		    $address = $event['message']['address'];
+		    $uid = $event['source']['userId'];
+
+		    $messages = [
+
+			"type"=> "location",
+			"title"=> 'ตำแหน่งที่ต้องการตรวจสอบ',
+			"address"=> $address,
+			"latitude"=> $latitude,
+			"longitude"=> $longitude
+		    ];  
+
+		    // Get replyToken
+		    $replyToken = $event['replyToken'];
+
+		    // Make a POST Request to Messaging API to reply to sender
+		    $url = 'https://api.line.me/v2/bot/message/reply';
+		    $data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+			//'messages' => ["https://gispwaai.herokuapp.com/golf.jpg"],
+
+		    ];
+		    $post = json_encode($data);
+		    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+		    $ch = curl_init($url);
+		    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		    $result = curl_exec($ch);
+		    curl_close($ch);
+
+		    echo $result . "\r\n";  
+
+		}		
+		
 	}
 }
 echo "OK";
