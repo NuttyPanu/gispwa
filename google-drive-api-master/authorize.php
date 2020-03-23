8c2dcd3b703c5783eb1e7b4d35845720bd0d6e57
NuttyPanu <misterpanukorn@gmail.com>
2020-03-23 13:50:52 +0700

    Add files via upload

---
+++ google-drive-api-master/authorize.php
@@ -0,0 +1,35 @@
+<?php
+/*****************************************************************
+Created :  2017-03-16
+Author : Mr. Khwanchai Kaewyos (LookHin)
+E-mail : khwanchai@gmail.com
+Website : https://www.unzeen.com
+Facebook : https://www.facebook.com/LookHin
+Source Code On Github : https://github.com/LookHin/google-drive-api
+*****************************************************************/
+
+include_once("google-drive-api.class.php");
+
+$strClientId = "966028613815-5enm1d9no2optbeg11jqh0eovpq4n1mr.apps.googleusercontent.com";
+$strClientSecret = "UQAT-a771gG55ItJR2e9D4vo";
+
+// Init Drive Object
+$obj = new GoogleDriveApi($strClientId, $strClientSecret);
+
+if(empty($_GET['code'])){
+
+  $strAuthScope = "https://www.googleapis.com/auth/drive";
+  $strAuthorizetUrl = $obj->getAuthorizetUrl($strAuthScope);
+
+  header("Location: {$strAuthorizetUrl}");
+  exit;
+
+}else{
+
+  $strRefreshToken = $obj->getRefreshToken($_GET['code']);
+  print "Refrest Token = ".$strRefreshToken;
+
+}
+
+
+?>
