ca06f6b009b05b25d8172f3c15ca193e8eab5b0e
NuttyPanu <misterpanukorn@gmail.com>
2020-05-22 15:58:35 +0700

    Update gis-pwa.php

--- gis-pwa.php
+++ gis-pwa.php
@@ -187,6 +187,38 @@ if ( $_GET['send'] == 'location' )
 	$client->pushMessage1($uid, $text);
 }
 
+if ( $_GET['send'] == 'notify' )
+{
+	$key_noti='OKJrnIrqpS70Vzey8aw9O3Nfa2GbD1zVgmHvbaUsmNv';
+	$msg=$_GET['text'];
+	$chOne = curl_init(); 
+	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+	// SSL USE 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+	//POST 
+	curl_setopt( $chOne, CURLOPT_POST, 1); 
+	// Message 
+	curl_setopt( $chOne, CURLOPT_POSTFIELDS, $msg); 
+	//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+	//curl_setopt( $chOne, CURLOPT_POSTFIELDS, //"message=$message&imageThumbnail=https://gispwaai.herokuapp.com/hbd.jpg&imageFullsize=https://gispwaai.herokuapp.com/hbd.jpg"); 
+	//"message=$message");
+	// follow redirects 
+	curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
+	//ADD header array 
+	$headers = array( 'Content-type: application/x-www-form-urlencoded', "Authorization: Bearer $key_noti", ); 
+	curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+	//RETURN 
+	curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+	$result = curl_exec( $chOne ); 
+	//Check error 
+	if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
+	else { $result_ = json_decode($result, true); 
+	echo "status : ".$result_['status']; 
+	echo "message : ". $result_['message']; } 
+	//Close connect 
+	curl_close( $chOne ); 
+}
 
 
 // Get POST body content
