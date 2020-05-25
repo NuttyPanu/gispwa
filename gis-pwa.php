9801c040f1d4e8d2ef9f020c0f7b2a36d4a206fe
NuttyPanu <misterpanukorn@gmail.com>
2020-05-25 16:13:07 +0700

    Update gis-pwa.php

--- gis-pwa.php
+++ gis-pwa.php
@@ -3582,114 +3582,150 @@ function replyMsg($event, $client)
                 }
 
 
-		else if (preg_match('(#flex1|flex2|flex3|flex4)', $msg) === 1) {
+				else if (preg_match('(#flex1|flex2|flex3|flex4)', $msg) === 1) {
 
-			$str ='https://gisweb1.pwa.co.th/lineservice/line_register/register.php?id='.$uid;
-					$a = array(
-							array(
-								'type' => 'flex',
-								'altText' => 'Air Quality',
-								'contents'=> array(
+					if (chk_friend($uid) == true){
+						//$gid = $event['source']['groupId'];
+						$uid = $event['source']['userId'];
 
-								/* เอามาจากflex*/
+						$url = 'https://api.line.me/v2/bot/profile/'.$uid;
+						//$url ='https://api.line.me/v2/bot/group/'.$gid.'/member/'.$uid;
+						$profile = get_profile($url);
+						$obj = json_decode($profile);
 
-								  "type"=> "bubble",
-								  "header"=> array(
-									"type"=> "box",
-									"layout"=> "horizontal",
-									"contents"=> array(
-									  array(
-										"type"=> "text",
-										"text"=> "ผูกบัญชีไลน์กับข้อมูลพนักงาน",
-										"color"=> "#414141",
-										"gravity"=> "center",
-										"size"=> "lg",
-										"wrap"=> true,
-										"align"=> "center"
-									  )
-									)
-								  ),
-								  "body"=> array(
-									"type"=> "box",
-									"layout"=> "vertical",
-									"contents"=> array(
-									  array(
-										"type"=> "box",
-										"layout"=> "horizontal",
-										"contents"=> array(
-										  array(
-											"type"=> "image",
-											"url"=> "https://www.iqair.com/assets/aqi/ic-face-green.svg",
-											"size"=> "md",
-											"align"=> "start"
-										  ),
-										  array(
-											"type"=> "text",
-											"text"=> "Moderate",
-											"wrap"=> true,
-											"size"=> "lg",
-											"color"=> "#a57f23",
-											"gravity"=> "center"
-										  )
-										),
-										"margin"=> "xxl"
-									  ),
-									  array(
-										"type"=> "box",
-										"layout"=> "baseline",
-										"contents"=> array(
-										  array(
-											"type"=> "text",
-											"text"=> "85",
-											"color"=> "#a57f23",
-											"size"=> "5xl",
-											"align"=> "center"
-										  ),
-										  array(
-											"type"=> "text",
-											"text"=> "US AQI",
-											"color"=> "#a57f23",
-											"size"=> "xs",
-											"margin"=> "sm"
-										  )
-										)
-									  ),
-									  array(
-										'type'=> "box",
-										'layout'=> "vertical",
-										'contents'=> array(
-										   array(
-											'type'=> "button",
-											'style'=> "primary",
-											'height'=> "sm",
-											'action'=> array(
-													'type'=> "uri",
-													'label'=> "Register",
-													'uri'=> $str
+						$nameid = $obj->displayName;
+						$status = $obj->statusMessage;
+						$pathpic = explode("cdn.net/", $obj->pictureUrl);
+						$iconUrl = 'https://obs.line-apps.com/'.$pathpic[1];
+
+
+						$str ='https://gisweb1.pwa.co.th/lineservice/line_register/register.php?id='.$uid;
+								$a = array(
+										array(
+											'type' => 'flex',
+											'altText' => 'Air Quality',
+											'contents'=> array(
+
+											/* เอามาจากflex*/
+
+											  "type"=> "bubble",
+											  "header"=> array(
+												"type"=> "box",
+												"layout"=> "horizontal",
+												"contents"=> array(
+												  array(
+													"type"=> "text",
+													"text"=> "ผูกบัญชีไลน์กับข้อมูลพนักงาน",
+													"color"=> "#414141",
+													"gravity"=> "center",
+													"size"=> "lg",
+													"wrap"=> true,
+													"align"=> "center"
+												  )
+												)
+											  ),
+											  "body"=> array(
+												"type"=> "box",
+												"layout"=> "vertical",
+												"contents"=> array(
+												  array(
+													"type"=> "box",
+													"layout"=> "horizontal",
+													"contents"=> array(
+													  array(
+														"type"=> "image",
+														"url"=> $iconUrl,
+														"size"=> "md",
+														"align"=> "start"
+													  ),
+													  array(
+														"type"=> "text",
+														"text"=> $nameid,
+														"wrap"=> true,
+														"size"=> "lg",
+														"color"=> "#a57f23",
+														"gravity"=> "end"
+													  )
+													),
+													"margin"=> "xxl"
+												  ),
+												  array(
+													"type"=> "box",
+													"layout"=> "baseline",
+													"contents"=> array(
+													  array(
+														"type"=> "text",
+														"text"=> "85",
+														"color"=> "#a57f23",
+														"size"=> "5xl",
+														"align"=> "center"
+													  ),
+													  array(
+														"type"=> "text",
+														"text"=> "US AQI",
+														"color"=> "#a57f23",
+														"size"=> "xs",
+														"margin"=> "sm"
+													  )
 													)
-										   )
-										  )
-										
-									  )
+												  ),
+
+												  array(
+													"type"=> "text",
+													"text"=> "",
+													"color"=> "#a57f23",
+													"size"=> "xs",
+													"align"=> "center"
+												  ),
+												  array(
+													'type'=> "box",
+													'layout'=> "vertical",
+													'contents'=> array(
+													   array(
+														'type'=> "button",
+														'style'=> "primary",
+														'height'=> "sm",
+														'action'=> array(
+																'type'=> "uri",
+																'label'=> "Register",
+																'uri'=> $str
+																)
+													   )
+													  )
+													
+												  )
 
 
+												)
+											  ),
+											  "styles"=> array(
+												"header"=> array(
+												  "backgroundColor"=> "#fdd74a"
+												),
+												"body"=> array(
+												  "backgroundColor"=> "#fff"
+												)
+											  )
+
+											/* เอามาจากflex*/
+
+											)
+										)
+								);
+								$client->replyMessage1($event['replyToken'],$a);
+
+
+					}
+					else{
+						$a = array(
+									array(
+										'type' => 'text',
+										'text' => 'โปรดเพิ่มบอทเป็นเพื่อนเก่อนการผูกบัญชี'          
 									)
-								  ),
-								  "styles"=> array(
-									"header"=> array(
-									  "backgroundColor"=> "#fdd74a"
-									),
-									"body"=> array(
-									  "backgroundColor"=> "#fdd74b"
-									)
-								  )
+								);
+						$client->replyMessage1($event['replyToken'],$a);
+					}
 
-								/* เอามาจากflex*/
-
-								)
-							)
-					);
-                    $client->replyMessage1($event['replyToken'],$a);
  
                 }
 
