bd57a138f363b1e30642ebfa4e6fad8103318a6a
NuttyPanu <misterpanukorn@gmail.com>
2020-05-26 20:45:03 +0700

    Update gis-pwa.php

--- gis-pwa.php
+++ gis-pwa.php
@@ -3606,6 +3606,67 @@ function replyMsg($event, $client)
 										   )
 										  )
 										
+									  ),          
+									  array(
+										"type"=> "box",
+										"layout"=> "vertical",
+										"contents"=> array(
+										  array(
+											"type"=> "spacer"
+										  )
+										)
+									  ),
+									  array(
+										'type'=> "box",
+										'layout'=> "vertical",
+										'contents'=> array(
+										   array(
+											'type'=> "button",
+											'style'=> "primary",
+											'height'=> "sm",
+											'action'=> array(
+													'type'=> "uri",
+													'label'=> "Register",
+													'uri'=> 'https://gis.pwa.co.th/home.php'
+													)
+										   )
+										  )
+										
+									  ),
+									  array(
+										"type"=> "box",
+										"layout"=> "vertical",
+										"contents"=> array(
+										  array(
+											"type"=> "spacer"
+										  )
+										)
+									  ),
+									  array(
+										'type'=> "box",
+										'layout'=> "vertical",
+										'contents'=> array(
+										   array(
+											'type'=> "button",
+											'style'=> "primary",
+											'height'=> "sm",
+											'action'=> array(
+													'type'=> "uri",
+													'label'=> "Register",
+													'uri'=> 'https://gis.pwa.co.th/home.php'
+													)
+										   )
+										  )
+										
+									  ),
+									  array(
+										"type"=> "box",
+										"layout"=> "vertical",
+										"contents"=> array(
+										  array(
+											"type"=> "spacer"
+										  )
+										)
 									  )
 
 									)
@@ -3616,9 +3677,6 @@ function replyMsg($event, $client)
 									),
 									"body"=> array(
 									  "backgroundColor"=> "#fffcf2"
-									),
-									"body"=> array(
-									  "backgroundColor"=> "#fdd74a"
 									)
 								  )
 
