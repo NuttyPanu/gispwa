722c5fba367c884f163338aeffc0695350d071fe
NuttyPanu <misterpanukorn@gmail.com>
2020-05-29 20:20:56 +0700

    Update gis-pwa.php

--- gis-pwa.php
+++ gis-pwa.php
@@ -5863,7 +5863,7 @@ array(
 						$a = array(
 									array(
 										'type' => 'text',
-										'text' => 'อยู่ระหว่างปรับปรุงระบบครับ'.$obj.data.city       
+										'text' => 'อยู่ระหว่างปรับปรุงระบบครับ'.$obj->data->city       
 									)
 								);
 						$client->replyMessage1($event['replyToken'],$a);
