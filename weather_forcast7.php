cc51c88ef25696d1914ae58562afb7edacc005a2
NuttyPanu <misterpanukorn@gmail.com>
2020-06-01 17:03:56 +0700

    Add files via upload

--- weather_forcast7.php
+++ weather_forcast7.php
@@ -36,7 +36,7 @@
 $data = "http://alerts.weather.gov/cap/tx.php?x=1";
 $entries = file_get_contents($data);
 $entries = new SimpleXmlElement($entries);
-if(count($entries)):
+if(count($entries)){
     //echo "<pre>";print_r($entries);die;
     //alternate way other than registring NameSpace
     //$asin = $asins->xpath("//*[local-name() = 'ASIN']");
@@ -45,13 +45,17 @@ if(count($entries)):
     $result = $entries->xpath("//prefix:entry");
     //echo count($asin);
     //echo "<pre>";print_r($result);die;
-    foreach ($result as $entry):
+    foreach ($result as $entry){
         //echo "<pre>";print_r($entry);die;
         $dc = $entry->children('urn:oasis:names:tc:emergency:cap:1.1');
         echo $dc->event."<br/>";
         echo $dc->effective."<br/>";
         echo "<hr>";
-    endforeach;
-endif;
+    }
+}
+else{
+
+
+}
 
 ?>
