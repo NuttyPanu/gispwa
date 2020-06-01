a0f2598ebf6ab5104d82c1d45cbacdf3038e0db4
NuttyPanu <misterpanukorn@gmail.com>
2020-06-02 00:33:39 +0700

    Add files via upload

--- gis-pwa.php
+++ gis-pwa.php
@@ -5361,7 +5361,7 @@ function replyMsg($event, $client)
 															)
 														  )
 														)
-													  ),
+													  )/*,
 													  array(
 														"type"=> "box",
 														"layout"=> "vertical",
@@ -5571,7 +5571,7 @@ function replyMsg($event, $client)
 															)
 														  )
 														)
-													  )/*,
+													  ),
 
 													  array(
 														"type"=> "box",
