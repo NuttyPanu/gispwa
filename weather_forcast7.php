0a2212f71a882ee03364999cec5bd4481dce74cc
NuttyPanu <misterpanukorn@gmail.com>
2020-06-01 16:44:08 +0700

    Add files via upload

--- weather_forcast7.php
+++ weather_forcast7.php
@@ -35,8 +35,8 @@
 			$fileContents= file_get_contents($url);
 			$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
 			$fileContents = trim(str_replace('"', "'", $fileContents));
-			$simpleXml = simplexml_load_string($fileContents);
-			$json = json_encode($simpleXml);
+			//$simpleXml = simplexml_load_string($fileContents);
+			$json = json_encode($fileContents);
 
 
 			header('Content-Type: application/json; charset=utf-8');
