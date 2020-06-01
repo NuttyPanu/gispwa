7afbff5f07b40b5b9a5fd07a2b4e9087f32e57cf
NuttyPanu <misterpanukorn@gmail.com>
2020-06-01 16:29:18 +0700

    Add files via upload

---
+++ weather_forcast7.php
@@ -0,0 +1,36 @@
+<?php
+
+		$fullurl = 'https://data.tmd.go.th/api/WeatherForecast7Days/V1/';
+
+		/*
+        $AccessToken = 'BwGlN2OsLJCMUF/YoAdKJR5GwQYCxU3BLTIPkMLLPC1IjVPH-S2LhD_P0R~GiPKA=JOP8N7MeY1U1ArA3X$LAQ0XfOiVsVxVUO2';
+ 
+        $header = array(
+            "Content-Type: application/json",
+            'Authorization: Token '.$AccessToken,
+        );
+		*/
+         
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		//curl_setopt( $ch, CURLOPT_POST, 1); //POST 
+        //curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+        //curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_URL, $fullurl);
+         
+        $returned =  curl_exec($ch);
+        curl_close($ch);
+
+
+		$xml = simplexml_load_string($returned);
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
+		echo $json;
+
+
+
+
+?>
