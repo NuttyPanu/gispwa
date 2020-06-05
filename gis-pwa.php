7c631cf5b305a3449b8b309ab7887b75e334e207
NuttyPanu <misterpanukorn@gmail.com>
2020-06-05 12:41:57 +0700

    Add files via upload

--- gis-pwa.php
+++ gis-pwa.php
@@ -2882,8 +2882,17 @@ function replyMsg($event, $client)
 						$urllink = 'https://gisweb1.pwa.co.th/lineservice/pwa_location/get_office_bot.php?name='.$pwa_name; 
 						//$urllink = 'https://gisweb1.pwa.co.th/bot_line/service/get_office_bot.php?pwa_code='.$pwacode; 
 						$str = get_url($urllink); //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+							$a_ = array(
 
-						if($str == 'ไม่พบตำแหน่ง'){
+										array(
+											'type' => 'text',
+											'text' => $str							
+										)
+									);
+							$client->replyMessage1($event['replyToken'],$a_);	
+
+
+						if($str == 'notfound'){
 
 							$a_ = array(
 
