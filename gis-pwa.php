26b04048bbcfa7b918370c94870c9c40ea0c3088
NuttyPanu <misterpanukorn@gmail.com>
2020-06-06 22:47:11 +0700

    Add files via upload

--- gis-pwa.php
+++ gis-pwa.php
@@ -7968,13 +7968,13 @@ function replyMsg($event, $client)
 									 }
 									*/
 
-									$txt =	'เลขพัสดุ: '.$value[count($value)-1]->barcode.' 
-									\n'.'สถานะ: '.$value[count($value)-1]->status_description.' 
-									\n'.'สถานที่: '.$value[count($value)-1]->location.' 
-									\n'.'วันที่: '.$value[count($value)-1]->status_date.' 
-									\n'.'สถานะการรับ: '.$value[count($value)-1]->delivery_description.' 
-									\n'.'วันที่รับ: '.$value[count($value)-1]->delivery_datetime.' 
-									\n'.'ผู้รับ: '.$value[count($value)-1]->receiver_name;
+									$txt =	'เลขพัสดุ: '.$value[count($value)-1]->barcode.'
+									\n '.'สถานะ: '.$value[count($value)-1]->status_description.'
+									\n '.'สถานที่: '.$value[count($value)-1]->location.'
+									\n '.'วันที่: '.$value[count($value)-1]->status_date.'
+									\n '.'สถานะการรับ: '.$value[count($value)-1]->delivery_description.'
+									\n '.'วันที่รับ: '.$value[count($value)-1]->delivery_datetime.'
+									\n '.'ผู้รับ: '.$value[count($value)-1]->receiver_name;
 											
 									//$link = 'https://trackimage.thailandpost.co.th/f/signature/QDUwMjQwYjVzMGx1VDMz/QGI1c0VCMGx1VDMx/QGI1czBsVEh1VDM0/QGI1czBsdTMxNTBUMzI';
 									$a = array(
